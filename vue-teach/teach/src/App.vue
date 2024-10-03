7d2dd2c55b34fe0b09552b7667ee51dea2d662d1
tomnyson <tabletkindfire@gmail.com>
2024-10-03 10:59:00 +0700

    update update

--- vue-teach/teach/src/App.vue
+++ vue-teach/teach/src/App.vue
@@ -6,17 +6,18 @@ import PostList from './components/PostList.vue';
 import DemoCompositionAPI from './components/DemoCompositionAPI.vue';
 import QuanLyCongViec from './components/QuanLyCongViec.vue';
 import LifeCycle from './components/LifeCycle.vue';
+import Blog from './screens/Blog.vue';
+import BlogDetail from './screens/BlogDetail.vue';
 </script>
 
 <template>
-  <div class="container">
-    <!-- <HelloWorld msg="Hello tomnyson!" />
+  <!-- <HelloWorld msg="Hello tomnyson!" />
       <XuatThongTin mssv="pk001" ten="Lê Hồng Sơn" lop="soa308" /> -->
-    <!-- <PostList /> -->
-    <!-- <DemoCompositionAPI /> -->
-    <!-- <QuanLyCongViec /> -->
-    <LifeCycle />
-  </div>
+  <!-- <PostList /> -->
+  <!-- <DemoCompositionAPI /> -->
+  <!-- <QuanLyCongViec /> -->
+  <!-- <LifeCycle /> -->
+  <BlogDetail />
 </template>
 
 <style scoped></style>
--- vue-teach/teach/src/main.js
+++ vue-teach/teach/src/main.js
@@ -6,5 +6,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/styles.css"
+import "./assets/js/scripts"
 
 createApp(App).mount('#app')
